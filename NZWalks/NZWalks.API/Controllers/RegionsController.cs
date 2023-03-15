89b92417405d7e5154a7f23db29c4619abb9ceeb
StevenHelliwell <stevehelliwell12@gmail.com>
2023-03-15 22:10:14 +0000

    removed static list

---
+++ NZWalks/NZWalks.API/Controllers/RegionsController.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using NZWalks.API.Models.Domain;
+using NZWalks.API.Repositories;
+
+namespace NZWalks.API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+
+    public class RegionsController : Controller
+    {
+        private readonly IRegionRepository regionRepository;
+
+        public RegionsController(IRegionRepository regionRepository)
+        {
+            this.regionRepository = regionRepository;
+        }
+        [HttpGet] 
+        public IActionResult GetAllRegions() {
+
+            /* var regions = new List<Region>()                         //this is a static list, this will be removed because
+                {                                                          we are getting data from the database          
+                    new Region
+                    {
+                        Id = Guid.NewGuid(),
+                        Name = "Wellington",
+                        Code = "WLG",
+                        Area = 227755,
+                        Lat = -1.8822,
+                        Long = 299.88,
+                        Population = 500000
+                    },
+                    new Region
+                    { 
+                        Id = Guid.NewGuid(),
+                        Name = "Auckland",
+                        Code = "AUCK",
+                        Area = 227755,
+                        Lat = -1.8822,
+                        Long = 299.88,
+                        Population = 500000
+
+                    }
+
+                };
+                */
+            var regions = regionRepository.GetAll();
+            return Ok(regions); 
+        }
+
+    }
+}
